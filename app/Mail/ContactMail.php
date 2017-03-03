e1460b4ff6f0239b65bc1790d8c5f5b94e57ac1e
Hasan Setiawan <vizzlearn@gmail.com>
2017-03-03 11:31:00 +0700

    add crojob for mailer

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+    * @variable Array(mail_data) $data
+    */
+    private $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.contact')
+                    ->with($this->data)
+                    ->replyTo($this->data['email'], $this->data['name'])
+                    ->subject('Blog Contact Form: '.$this->data['name']);
+    }
+}
