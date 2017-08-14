5e8b57fd1b8574147d4da0ec8562c6d5859ef53b
vizzlearn@gmail.com <vizzlearn@gmail.com>
2017-08-14 11:51:06 +0700

    add stuff

--- resources/views/blog/contact.blade.php
+++ resources/views/blog/contact.blade.php
@@ -7,7 +7,7 @@
       <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
           <div class="site-heading">
-            <h1>Contact Me</h1>
+            <h1>Contact Us</h1>
             <hr class="small">
             <h2 class="subheading">
               Have questions? I have answers (maybe).
